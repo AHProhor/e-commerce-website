f5202adb5e7d8642f86de913fc5bcc06df1f237f
PROHOR <ahprohor88@gmail.com>
2021-11-11 23:23:48 +0600

    first commit

---
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div>
+            <h2>THis is Navigation</h2>
+        </div>
+    );
+};
+
+export default Navigation;
